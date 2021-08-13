590352a3142b30823b71fafdb9d1636f86cd2a2a
Shelvin Liu <375577756@qq.com>
2021-08-13 16:39:16 +1200

    Update jsondatareader.py

--- datareader/jsondatareader.py
+++ datareader/jsondatareader.py
@@ -2,16 +2,28 @@ import json
 
 class BooksJSONReader:
 
-    def __init__(self, books_file_name: str, authors_file_name: str):
-        # TODO
+    def __init__(self, books_file_name, authors_file_name):
         self.__books_file_name = None
         self.__authors_file_name = None
         self.__dataset_of_books = None
-
+        if isinstance(books_file_name, str) and len(books_file_name.strip())>-1:
+            self.__books_file_name = books_file_name
+        else:
+            raise ValueError()
+        if isinstance(authors_file_name, str) and len(authors_file_name.strip())>-1:
+            self.__authors_file_name = authors_file_name
+        else:
+            raise ValueError()
     @property
-    def dataset_of_books(self) -> list:
+    def dataset_of_books(self):
         return self.__dataset_of_books
-
+        
     def read_json_files(self):
-        # TODO
-        pass
+        try:
+            with open(self.__authors_file_name, "r") as read_file:
+            with open(self.__books_file_name, "r") as book_file:
+            authordata = json.load(read_file)
+            bookdata = json.load(book_file)
+            
+        except ValueError:
+            print()
